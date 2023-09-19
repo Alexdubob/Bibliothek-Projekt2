2cf78db46ce1be5571c7aaeae52571c1eff066c2
Alexdubob <129725495+Alexdubob@users.noreply.github.com>
2023-09-19 15:22:31 +0200

    basic layout
    card basics

--- Bibliothek-Project/src/app/app.component.ts
+++ Bibliothek-Project/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+cardTexts =[
+  "Dies ist ein Test Text1",
+  "Dies ist ein Test Text2",
+  "Dies ist ein Test Text3",
+  "Dies ist ein Test Text4"
+];
+
+cardImages = [
+  "../../assets/img/img1.jpg",
+  "../../assets/img/img2.jpg",
+  "../../assets/img/img3.jpg",
+  "../../assets/img/img4.jpg",
+  
+]
+
   title = 'Bibliothek-Project';
 }
+
